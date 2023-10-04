c20e381dad64ee74115fbb51ca86401a0b9833ff
Rakshit <rakshitsahai@gmail.com>
2023-10-04 21:04:33 +0530

    added Note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import Note from "./Note";
 
 function App() {
   return (
     <React.StrictMode>
       <Header />
+      <Note />
       <Footer />
     </React.StrictMode>
   );
